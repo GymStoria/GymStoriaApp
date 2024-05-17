22d199f2a29ddd95218d6c8c376c119fe944447b
andika febriansyah <82563655+afebrii@users.noreply.github.com>
2024-05-17 22:30:28 +0700

    add loginscreen

--- app/src/main/java/com/afebrii/gymstoriaapp/ui/MainActivity.kt
+++ app/src/main/java/com/afebrii/gymstoriaapp/ui/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.afebrii.gymstoriaapp.Screen.LoginScreen
 import com.afebrii.gymstoriaapp.screen.RegisterScreen
 import com.afebrii.gymstoriaapp.ui.theme.GymStoriaAppTheme
 
@@ -18,25 +20,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContent {
-                RegisterScreen()
+                LoginScreen()
             }
         }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GymStoriaAppTheme {
-        Greeting("Android")
-    }
-}
-
-// dhia irsyad
